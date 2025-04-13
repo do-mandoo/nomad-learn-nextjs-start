0eeb4b5a23d00392bc7815d4209fb62f550945af
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2025-04-13 09:36:09 +0900

    setting-commit

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Navigation from './about-us/company/navigation';
+
+export default function NotFound() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Not FOund!!</h1>
+    </div>
+  );
+}
